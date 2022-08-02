b68679058b26886a1ea4ed2d4de0e679c5cd072b
Amritvir Randhawa <arandhawa@mail.csuchico.edu>
2022-08-01 21:10:17 -0700

    Display posts in Home Screen

---
+++ lib/Pages/snapix.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:pixapp/widgets/post_widgets.dart';
+
+class SnapixPage extends StatefulWidget {
+  const SnapixPage({Key? key}) : super(key: key);
+
+  @override
+  State<SnapixPage> createState() => _SnapixPageState();
+}
+
+class _SnapixPageState extends State<SnapixPage> {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (ctx, index) => 
+            PostSnapix(
+              output: snapshot.data!.docs[index].data(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
